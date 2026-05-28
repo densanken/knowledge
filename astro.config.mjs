1cbccbde1c4101ed4038c313e5337c16f5c2702c
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2026-05-28 17:28:54 +0900

    chore: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -7,4 +8,7 @@ export default defineConfig({
   output: "server",
   integrations: [react()],
   adapter: cloudflare(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
