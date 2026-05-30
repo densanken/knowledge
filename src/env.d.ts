77a9850bffb76d81a45142a3f7463b12f680ef38
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2026-05-31 06:07:25 +0900

    refactor: remove redundant import meta env declarations

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,1 @@
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-  readonly BETTER_AUTH_SECRET: string;
-  readonly BETTER_AUTH_URL: string;
-  readonly DISCORD_CLIENT_ID: string;
-  readonly DISCORD_CLIENT_SECRET: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
