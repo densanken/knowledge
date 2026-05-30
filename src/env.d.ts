e0985f5dfa57c2515cba56b4ea630b390aaecc87
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2026-05-31 05:59:15 +0900

    fix: remove optional attribute of env vars

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly BETTER_AUTH_SECRET?: string;
-  readonly BETTER_AUTH_URL?: string;
-  readonly DISCORD_CLIENT_ID?: string;
-  readonly DISCORD_CLIENT_SECRET?: string;
+  readonly BETTER_AUTH_SECRET: string;
+  readonly BETTER_AUTH_URL: string;
+  readonly DISCORD_CLIENT_ID: string;
+  readonly DISCORD_CLIENT_SECRET: string;
 }
 
 interface ImportMeta {
