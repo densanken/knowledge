4a07a4f1e0c3833764cfb053c35b314e1310b286
ryosanhin <90621212+ryosanhin@users.noreply.github.com>
2026-07-24 21:20:35 +0900

    change: 変数をdocs系に統一、ページタイトルの形式を統一

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,22 +2,21 @@ import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 import { defineCollection } from "astro:content";
 
-// なんとなくinternal-docs側で定義していても使えそうな形でテスト
-export const basicPostSchema = z
+export const basicDocsSchema = z
   .object({
     title: z.string(),
     description: z.string().optional(),
   })
   .strict();
 
-const allPosts = defineCollection({
+const docs = defineCollection({
   loader: glob({
     pattern: "**/[^_]*.{md,mdx}",
     base: "./internal-docs/docs",
   }),
-  schema: basicPostSchema,
+  schema: basicDocsSchema,
 });
 
 export const collections = {
-  allPosts,
+  docs,
 };
